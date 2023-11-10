0fa89f9fa1bde2d20d68a1f717af9b69d920ac4b
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-11-10 12:13:32 -0300

    remove my password

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
+import { email, password } from './password'
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require("mongoose")
 const crypto = require('crypto')
 const nodemailer = require('nodemailer')
+const cors = require('cors')
+
+const app = express()
+const port = 8000
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended:false}))
+app.use(bodyParser.json())
+
+mongoose.connect(`mongodb+srv://${email}:<${password}>@cluster0.scqfhk3.mongodb.net/`)
