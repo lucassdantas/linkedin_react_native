9f1aa1ca0cde29360c24486a523a568daa29082f
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-11-10 16:14:14 -0300

    add password to login

--- app/{authenticate}/login.js
+++ app/{authenticate}/login.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View, SafeAreaView, Image, KeyboardAvoidingView, TextInput } from 'react-native'
 import React from 'react'
 import {MaterialIcons} from '@expo/vector-icons'
+import {AntDesign} from '@expo/vector-icons'
 const login = () => {
   return (
     <SafeAreaView style={{flex:1, backgroundColor:'white', alignItems:'center'}}>
@@ -30,8 +31,32 @@ const login = () => {
             }}
           >
             <MaterialIcons style={{marginLeft:8}} name='email' size={24} 
-            color='black'/>
-            <TextInput style={{color:'gray', marginVertical:10, width:300}} placeholder='Insira seu e-mail' />
+            color='gray'/>
+            <TextInput
+              secureTextEntry={true}
+              style={{
+                color:'gray', 
+                marginVertical:10, 
+                width:300
+              }} 
+              placeholder='Insira seu e-mail' />
+          </View>
+        </View>
+
+        <View style={{marginTop:10}}>
+          <View 
+            style={{
+              flexDirection:'row', alignItems:'center',
+              gap:5, 
+              backgroundColor:'#E0E0E0',
+              paddingVertical:5,
+              borderRadius:5,
+              marginTop:30
+            }}
+          >
+            <AntDesign style={{marginLeft:8}} name='lock1' size={24} 
+            color='gray'/>
+            <TextInput style={{color:'gray', marginVertical:10, width:300}} placeholder='Insira sua senha' />
           </View>
         </View>
       </KeyboardAvoidingView>
